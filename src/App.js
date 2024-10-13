4c6a1de6dd2b49d8e56fc9a3150b802a1a0e752c
blloop <42186044+blloop@users.noreply.github.com>
2024-10-12 20:29:49 -0700

    add base layout, remove extras

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center size-full">
+      <button className="bg-red-500 text-white rounded-full px-4 py-2 md:hover:bg-red-700 transition-colors">
+        Animate!
+      </button>
     </div>
   );
 }
