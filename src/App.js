3b09b296fa51ee7cfd871b4b0dd436fca443b8c0
blloop <42186044+blloop@users.noreply.github.com>
2024-10-12 23:22:16 -0700

    add sample list with scrolling background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Panel from "./components/panel";
+import Ball from "./ball.png";
 
 function App() {
   const [canUse, setCanUse] = useState(true);
+  const [scrollY, setScrollY] = useState(0);
 
   const showPanels = () => {
     if (!canUse) return;
@@ -18,17 +20,50 @@ function App() {
     }, 1500);
   };
 
+  const handleScroll = (e) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target;
+    const position = Math.ceil(
+      (scrollTop / (scrollHeight - clientHeight)) * 100,
+    );
+    setScrollY(position);
+  };
+
   return (
-    <div className="flex items-center justify-center size-full">
+    <div className="flex items-center justify-center size-full p-8 bg-[#fefece] overflow-hidden">
       <Panel />
-      <button
-        onClick={showPanels}
-        className="bg-red-500 text-white rounded-full px-4 py-2 md:hover:bg-red-700 transition-colors"
+      <div className="flex flex-col items-center gap-8 w-1/2">
+        <div className="bg-[#e7dfa1] w-1/2 h-2" />
+        <button
+          onClick={showPanels}
+          className="bg-red-500 text-white rounded-full px-4 py-2 md:hover:bg-red-700 transition-colors"
+        >
+          Refresh!
+        </button>
+        <div className="bg-[#e7dfa1] w-1/2 h-2" />
+      </div>
+      <div
+        className="w-1/2 p-4 border-2 border-black h-full overflow-y-auto z-10"
+        onScroll={handleScroll}
       >
-        Animate!
-      </button>
+        {Array.from(Array(202).keys()).map((e) => (
+          <p className="text-3xl" key={e}>
+            No{e.toString().padStart(3, "0")}
+          </p>
+        ))}
+      </div>
+      <div className="absolute right-[5vw] top-[25vh] w-[50vh] opacity-25 overflow-hidden">
+        <img
+          src={Ball}
+          className="size-full"
+          alt=""
+          style={{ transform: `rotate(${scrollY * 5}deg)` }}
+        />
+      </div>
     </div>
   );
 }
+
+// FEFECE light tan
+// E7DFA1 dark tan
 
 export default App;
--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -1,12 +1,18 @@
 export default function Panel() {
   return (
-    <div className="absolute inset-0 overflow-hidden pointer-events-none">
-      <div id="panel-top" className="relative top-[-50vh] w-full h-1/2 bg-[#f0463d] border-b-4 border-black transition-top duration-500 pointer-events-auto">
-        <div className="absolute left-[calc(50vw-4.5rem)] top-[calc(100%-4.25rem)] w-36 h-[4.5rem] bg-white border-8 border-black border-b-white rounded-t-full" />
+    <div className="absolute inset-0 overflow-hidden pointer-events-none z-40">
+      <div
+        id="panel-top"
+        className="relative top-[-50vh] w-full h-1/2 bg-[#f0463d] border-b-8 border-black transition-top duration-500 pointer-events-auto"
+      >
+        <div className="absolute left-[calc(50vw-4.5rem)] top-[calc(100%-4rem)] w-36 h-[4.5rem] bg-white border-[1rem] border-black border-b-0 rounded-t-full" />
       </div>
-      <div id="panel-bottom" className="relative bottom-[-50vh] w-full h-1/2 bg-white border-t-4 border-black transition-bottom duration-500 pointer-events-auto">
-        <div className="absolute left-[calc(50vw-4.5rem)] bottom-[calc(50vh-4.25rem)] w-36 h-[4.5rem] bg-white border-8 border-black border-t-white rounded-b-full" />
+      <div
+        id="panel-bottom"
+        className="relative bottom-[-50vh] w-full h-1/2 bg-white border-t-8 border-black transition-bottom duration-500 pointer-events-auto"
+      >
+        <div className="absolute left-[calc(50vw-4.5rem)] bottom-[calc(50vh-4.5rem)] w-36 h-[4.5rem] bg-white border-[1rem] border-black border-t-0 rounded-b-full" />
       </div>
     </div>
-  )
+  );
 }
