198b692143003f6322b1cc4e7a7d91de5b5e4d38
blloop <42186044+blloop@users.noreply.github.com>
2024-10-24 19:33:05 -0700

    update background styles, add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
       colors: {
         // pokered: "#dc0a2d",
         pokered: "#f63920",
-        pokeblack: "#212121",
-        pokewhite: "#dbdbdb",
+        pokeblack: "#181818",
+        pokegray: "#dbdbdb",
+        pokewhite: "#efefef",
         dark: "#383838",
         light: "#e4e4e4",
       },
@@ -17,9 +18,13 @@ module.exports = {
       },
       backgroundImage: {
         tiles: "url('/public/tiles.png')",
+        tilesBlack: "url('/public/tiles-black.png')",
       },
       dropShadow: {
         gray: "2px 2px 1px #888888",
+      },
+      boxShadow: {
+        window: "inset 0px 0px 4px 1px #444",
       },
     },
   },
