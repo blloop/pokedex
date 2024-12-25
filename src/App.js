6edaa73cd01c8658f0a0d04e582e0897a7ce6cb6
blloop <42186044+blloop@users.noreply.github.com>
2024-12-24 18:58:27 -0800

    add proportional scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import Panel from "./components/panel";
 import Window from "./components/window";
 import Names from "./data/names.json";
@@ -9,6 +9,9 @@ import ArrowLeft from "./assets/arrow-left.png";
 import ArrowRight from "./assets/arrow-right.png";
 import Frame from "./assets/frame.png";
 import { cn } from "./utils";
+
+// TODO: Check if index is selected when clicking list item
+// and set index if not equal
 
 // Sprites and icons credit: https://veekun.com/dex/downloads
 
@@ -25,35 +28,35 @@ function App() {
   const scrollRef = useRef(null);
   const handleScroll = useCallback(() => {
     if (!scrollRef.current) return;
-    const children = Array.from(scrollRef.current.children);
-    const rect = scrollRef.current.getBoundingClientRect();
-    const center = rect.top + rect.height / 2;
-    
-    let closestIndex = 0;
-    let closestDistance = Infinity;
-    children.forEach((child, index) => {
-      const r = child.getBoundingClientRect();
-      const middle = r.top + r.height / 2;
-      const distance = Math.abs(middle - center);
-      if (distance < closestDistance) {
-        closestDistance = distance;
-        closestIndex = index;
-      }
-    });
-  
-    setIndex((prevIndex) => (prevIndex !== closestIndex ? closestIndex : prevIndex));
-  }, []);
+
+    const container = scrollRef.current;
+    const scrollTop = container.scrollTop;
+    const clientHeight = container.clientHeight;
+    const scrollHeight = container.scrollHeight;
+
+    // Calculate the proportional target index based on the current scroll position
+    const proportionalIndex =
+      (scrollTop / (scrollHeight - clientHeight)) * (Names[game].length - 1);
+    console.log(
+      scrollTop / (scrollHeight - clientHeight),
+      game,
+      Names[game].length,
+    );
+    const targetIndex = Math.ceil(proportionalIndex);
+    setIndex(targetIndex);
+  }, [game]);
 
   const setScroll = (create) => {
     const container = scrollRef.current;
     if (container) {
       if (create) {
+        setIndex(0);
         container.addEventListener("scroll", handleScroll);
       } else {
         container.removeEventListener("scroll", handleScroll);
       }
     }
-  }
+  };
 
   const gameList = [
     "Red/Blue",
@@ -112,20 +115,28 @@ function App() {
         onClick={() => navigate(2, game)}
         className={cn(
           "group relative flex justify-between md:justify-start items-center gap-2 md:gap-4 md:hover:bg-limeDark md:hover:text-limeLight text-pokegray text-shadow-dark bg-pokeblack overflow-visible",
-          number === index ? "bg-limeDark text-limeLight" : ""
+          number === index ? "bg-limeDark text-limeLight" : "",
         )}
         key={number}
       >
-        <div className={cn("w-0 h-0 border-t-[18px] md:border-t-[24px] border-t-transparent border-r-[12px] md:border-r-[12px] border-r-pokeblack md:group-hover:border-r-limeDark border-b-[18px] md:border-b-[24px] border-b-transparent absolute -left-3",
-          number === index ? "border-r-limeDark" : ""
-        )} />
-        <div className={cn("w-0 h-0 border-t-[36px] md:border-t-[48px] border-t-pokeblack border-r-[16px] md:border-r-[25px] border-r-transparent md:group-hover:border-t-limeDark absolute -right-[15px] md:-right-6",
-          number === index ? "border-t-limeDark" : ""
-        )} />
-        <div className={cn(
-          "relative -left-2 -top-0 shrink-0 h-8 md:h-12 md:group-hover:brightness-125",
-          number === index ? "brightness-125" : ""
-        )}>
+        <div
+          className={cn(
+            "w-0 h-0 border-t-[18px] md:border-t-[24px] border-t-transparent border-r-[12px] md:border-r-[12px] border-r-pokeblack md:group-hover:border-r-limeDark border-b-[18px] md:border-b-[24px] border-b-transparent absolute -left-3",
+            number === index ? "border-r-limeDark" : "",
+          )}
+        />
+        <div
+          className={cn(
+            "w-0 h-0 border-t-[36px] md:border-t-[48px] border-t-pokeblack border-r-[16px] md:border-r-[25px] border-r-transparent md:group-hover:border-t-limeDark absolute -right-[15px] md:-right-6",
+            number === index ? "border-t-limeDark" : "",
+          )}
+        />
+        <div
+          className={cn(
+            "relative -left-2 -top-0 shrink-0 h-8 md:h-12 md:group-hover:brightness-125",
+            number === index ? "brightness-125" : "",
+          )}
+        >
           <img className="absolute left-0 h-full" src={ArrowLeft} alt="" />
           <img className="absolute left-12 h-full" src={ArrowRight} alt="" />
           <div className="w-16 h-full overflow-hidden">
@@ -149,16 +160,23 @@ function App() {
       case 1:
         return (
           <>
-            <div className="z-10 relative flex flex-col sm:flex-row justify-between gap-4 md:gap-8 px-4 md:px-8 size-full overflow-y-auto">
-              <div className="relative flex flex-col justify-center items-center w-full h-auto">
-                <img src={Frame} className="w-full max-w-96" alt="" />
-                <img src={`/sprites/${Mapping[game][index]}.png`} className="absolute top-0 w-full max-w-96"/>
+            <div className="z-10 relative flex flex-col sm:flex-row items-center justify-between gap-4 md:gap-8 px-4 md:px-8 size-full overflow-y-auto">
+              <div className="relative flex flex-col justify-center items-center w-auto sm:w-full h-96 sm:h-auto">
+                <img src={Frame} className="size-full max-w-96" alt="" />
+                <img
+                  alt={Names[game][index]}
+                  src={`/sprites/${Mapping[game][index]}.png`}
+                  className="absolute w-full max-w-96 scale-x-[-1]"
+                />
               </div>
-              <div ref={scrollRef} className="size-full sm:shrink-0 sm:w-3/5 lg:w-1/2 flex flex-col gap-2 pl-4 pr-6 md:pr-8 overflow-y-auto z-10">
+              <div
+                ref={scrollRef}
+                className="size-full sm:shrink-0 sm:w-3/5 lg:w-1/2 flex flex-col gap-2 pl-4 pr-6 md:pr-8 overflow-y-auto z-10"
+              >
                 {Names[game].map((e, i) => renderListItem(e, i))}
               </div>
             </div>
-            <div className="absolute z-0 h-36 w-full top-[calc(50vh-6rem)] left-0 object-contain bg-stripes bg-repeat-x"></div>
+            <div className="absolute z-0 h-64 w-full top-[calc(50vh-8rem)] left-0 object-contain bg-stripes bg-contain bg-repeat-x"></div>
             <div
               id="hexrow"
               className="flex justify-end gap-2 w-full px-4 py-2 text-2xl text-white text-center bg-gradient-to-t from-pokeblack to-gray border-t-2 border-pokeblack"
