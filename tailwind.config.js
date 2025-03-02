4aa7cbb64f22eaeb0eff30f951288d8eac1af5be
blloop <42186044+blloop@users.noreply.github.com>
2025-03-02 14:55:08 -0800

    revert to tailwind v3.4.17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
