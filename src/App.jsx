ceedf4e33f1ca39bb88daead0d455669bf4624ba
blloop <42186044+blloop@users.noreply.github.com>
2025-04-03 19:28:24 -0700

    switch to back button only

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ import StatList from "./components/statList";
 import InfoList from "./components/infoList";
 
 import BackButton from "./assets/back.png";
-import CloseButton from "./assets/close.png";
 import ArrowLeft from "./assets/arrow-left.png";
 import ArrowRight from "./assets/arrow-right.png";
 import ArrowUp from "./assets/arrow-up.png";
@@ -657,46 +656,27 @@ function App() {
             {/* <HexButton isButton={screen !== 5} text={"DATA"} toScreen={5} /> */}
           </div>
         )}
-        <div className="flex items-center gap-4">
-          <button
-            className={cn(
-              "md:hover:brightness-50 transition-filter",
-              screen < 2 && "hidden",
-            )}
-            onClick={() => {
-              if (screen === 1) {
-                navigate(0);
-                setTimeout(() => {
-                  setMonster(0);
-                  setMove("");
-                }, FADE_MS * 1.5);
-              } else {
-                setTimeout(() => {
-                  scrollRef.current.scrollTop = position;
-                }, FADE_MS * 1.5);
-                navigate(1);
-              }
-            }}
-          >
-            <img className="h-8 sm:h-10" src={BackButton} alt="" />
-          </button>
-          <button
-            className="md:hover:brightness-50 transition-filter"
-            onClick={() => {
-              if (screen === 0) {
-                togglePanel(true);
-              } else {
-                navigate(0);
-                setTimeout(() => {
-                  setMonster(0);
-                  setMove("");
-                }, FADE_MS * 1.5);
-              }
-            }}
-          >
-            <img className="h-8 sm:h-10" src={CloseButton} alt="" />
-          </button>
-        </div>
+        <button
+          className="md:hover:brightness-50 transition-filter ml-10"
+          onClick={() => {
+            if (screen === 0) {
+              togglePanel(true);
+            } else if (screen === 1) {
+              navigate(0);
+              setTimeout(() => {
+                setMonster(0);
+                setMove("");
+              }, FADE_MS * 1.5);
+            } else {
+              setTimeout(() => {
+                scrollRef.current.scrollTop = position;
+              }, FADE_MS * 1.5);
+              navigate(1);
+            }
+          }}
+        >
+          <img className="h-8 sm:h-10" src={BackButton} alt="" />
+        </button>
       </div>
     </div>
   );
